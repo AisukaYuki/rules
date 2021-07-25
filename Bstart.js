997fa6b688d5d009c7d385585ac79f18a09b24f5
AisukaYuki <yubingruo@gmail.com>
2021-07-25 16:08:39 +0800

    Create Bstart.js

---
+++ Bstart.js
@@ -0,0 +1,9 @@
+let body = $request.body;
+
+body = body.replace(/&sim_code=\d+/,"&sim_code=51503");
+body = body.replace(/&locale=zh_CN/,"&&locale=zh_SG");
+body = body.replace(/&s_locale=zh-Hans_[A-Z]{2}/,"&s_locale=zh-Hans_SG");
+
+console.log(body)
+
+$done({body});
