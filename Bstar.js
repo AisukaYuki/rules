b8749fee83dcd24d35ec0727dd3055324a083cf0
AisukaYuki <yubingruo@gmail.com>
2021-11-29 00:14:50 +0800

    Update Bstar.js

--- Bstar.js
+++ Bstar.js
@@ -1,6 +1,6 @@
 let body = $request.body;
 
-body = body.replace(/&sim_code=\d+/,"&sim_code=");
+body = body.replace(/&sim_code=\d+/,"&sim_code=51503");
 body = body.replace(/&locale=zh_CN/,"&locale=zh_SG");
 body = body.replace(/&s_locale=zh-Hans_[A-Z]{2}/,"&s_locale=zh-Hans_SG");
 
