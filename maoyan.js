7f3019fd57f7b4667ec285d2ceee6bb35749a92f
AisukaYuki <yubingruo@gmail.com>
2024-05-09 01:42:18 +0800

    Update maoyan.js

--- maoyan.js
+++ maoyan.js
@@ -1,6 +1,6 @@
 let body = $response.body;
 //body = body.replace('"sellStatus":3','"sellStatus":1');
-body = body.replace(/"showStatus":\d/,'"showStatus":1');
+body = body.replace(/"showStatus":\d/,'"showStatus":0');
 
 console.log(body);
 $done({body});
