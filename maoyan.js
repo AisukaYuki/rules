1dfb1a90e37d0675f834fbb98a8ce7d281f6dd0d
AisukaYuki <yubingruo@gmail.com>
2024-05-09 01:51:36 +0800

    Update maoyan.js

--- maoyan.js
+++ maoyan.js
@@ -1,6 +1,7 @@
 let body = $response.body;
 body = body.replace('"hasInventory":false','"hasInventory":true');
 body = body.replace(/"showStatus":\d/,'"showStatus":0');
+body = body.replace('"stockable":false','"stockable":true');
 
 console.log(body);
 $done({body});
